e6ce0d77064475bbe077bf49923e82c800b2207e
Diptadeep-21 <sinhadiptadeep.official@gmail.com>
2025-07-19 14:09:28 +0530

    Update axios.js

--- Backend/utils/axios.js
+++ Backend/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+  baseURL: 'https://glide-way-backend.onrender.com/api', // Replace with your backend URL
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   },
