69f3d473172d9bff6c6f5aefb68e80c15ebc1af2
Diptadeep Sinha <sinhadiptadeep.official@gmail.com>
2026-01-17 13:31:27 +0530

    Updated Frontend URL

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://glide-way-frontend.onrender.com',
     methods: ['GET', 'POST'],
     credentials: true,
   },
